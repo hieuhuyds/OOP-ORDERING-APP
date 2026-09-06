e834f816ca7afdfb928033bca372fa2f0936234c
Yuyu192 <nguyenthu190205@gmail.com>
2026-09-06 21:33:50 +0700

    Cập nhật MusicProduct

--- cores/products/MusicProduct.h
+++ cores/products/MusicProduct.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Product.h"
+
+class MusicProduct : public Product {
+private:
+  string artist;
+  int durationMinutes;
+
+public:
+  MusicProduct(string id = "", string name = "", double price = 0,
+               int stockQuantity = 0, string artist = "",
+               int durationMinutes = 0);
+
+  string getArtist() const;
+  int getDurationMinutes() const;
+
+  void displayInfo() const override;
+  double calculateFinalPrice() const override;
+  string getType() const override;
+};
