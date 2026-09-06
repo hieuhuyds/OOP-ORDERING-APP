ddf20ba43c25c67c0629ffd2c25f1c19f4b510c9
Phamnguyen-tuongvy <phamtuongvii2020@gmail.com>
2026-09-06 22:13:36 +0700

    add GameProduct.cpp

--- cores/products/GameProduct.cpp
+++ cores/products/GameProduct.cpp
@@ -1,1 +1,38 @@
 #include "GameProduct.h"
+#include <iostream>
+using namespace std;
+GameProduct::GameProduct(string id, string name, double price, int stockQuantity, string platform, string genre)
+    : Product(id, name, price, stockQuantity, "Game"), platform(platform), genre(genre) {}
+
+string GameProduct::getPlatform() const {
+    return platform;
+}
+
+string GameProduct::getGenre() const {
+    return genre;
+}   
+
+void GameProduct::displayInfo() const 
+{
+    cout << "ID: " << id << endl;
+    cout << "Name: " << name << endl;
+    cout << "Price: $" << price << endl;
+    cout << "Stock Quantity: " << stockQuantity << endl;
+    cout << "Category: " << category << endl;
+    cout << "Platform: " << platform << endl;
+    cout << "Genre: " << genre << endl;
+}
+
+double GameProduct::calculateFinalPrice() const 
+{
+    if (platform == "Console")
+    {
+        return price * 1.05; 
+    }
+    return price;
+}
+
+string GameProduct::getType() const 
+{
+    return "Game";
+}
