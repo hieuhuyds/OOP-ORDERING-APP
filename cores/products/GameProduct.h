f09882e5518f88a30cb36e97a1b843115fb87eb7
Phamnguyen-tuongvy <phamtuongvii2020@gmail.com>
2026-09-06 22:22:57 +0700

    Update GameProduct.h

--- cores/products/GameProduct.h
+++ cores/products/GameProduct.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Product.h"
+#include <string>
+using namespace std;
+
+class GameProduct : public Product {
+private:
+    string platform;
+    string genre;
+public:
+    GameProduct(string id = "", string name = "", double price = 0.0, int stockQuantity = 0, string platform = "", string genre = "");
+
+    string getPlatform() const;
+    string getGenre() const;
+
+    void displayInfo() const override;
+    double calculateFinalPrice() const override;
+    string getType() const override;
+      
+};
