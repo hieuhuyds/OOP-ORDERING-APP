62d2c9f29eeb7ff4a53a124465f7a8c6ef6d3c75
Minh Tien <tranlest104@gmail.com>
2026-09-06 19:37:48 +0700

    feat: khai bao va cai dat BookProduct (constructor, getter, displayInfo, calculateFinalPrice, getType)

--- cores/products/BookProduct.h
+++ cores/products/BookProduct.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Product.h"
+
+class BookProduct : public Product
+{
+private:
+    string Author;
+    int Pages;
+
+public:
+    BookProduct(string id ="", string name="",double price= 0,int stockQuantity= 0,string author = "", int pages = 0);
+    string getAuthor() const;
+    int getPages() const;
+     void displayInfo() const override; 
+     double calculateFinalPrice() const override  ;
+     string getType() const override;
+};
