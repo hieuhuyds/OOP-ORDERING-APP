6024741910e61a74d258092607d03de40ef0aec7
Phamnguyen-tuongvy <phamtuongvii2020@gmail.com>
2026-09-08 01:10:09 +0700

    Update ShoppingCart.h

--- cores/cart/ShoppingCart.h
+++ cores/cart/ShoppingCart.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <vector>
+#include "CartItem.h"
+using namespace std;
+
+class ShoppingCart {
+    private:
+        vector<CartItem> items;
+public:
+    bool addProduct(const CartItem& item);
+    void removeProduct(const CartItem& item);
+    void displayCart() const;
+    double calculateSubtotal() const;
+    bool isEmpty() const;
+    void clear();
+    const vector<CartItem>& getItems() const;
+};
