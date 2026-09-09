796b108bef3ef4c2cebfff2df72bb490727eb283
Minh Tien <tranlest104@gmail.com>
2026-09-10 00:58:08 +0700

    Fix OrderManage checkout and displayOrderHistory methods

--- manage/OrderManage.h
+++ manage/OrderManage.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "../cores/order/Order.h"
+#include "../cores/cart/ShoppingCart.h"
+#include "ProductManage.h"
+#include "../cores/customer/Customer.h"
+#include <vector>
+#include <string>
+
+class OrderManage {
+private:
+    std::vector<Order> orders;
+
+public:
+    bool checkout(const Customer& customer, ShoppingCart& cart, ProductManage& productManage);
+    void displayOrderHistory() const;
+    std::string generateOrderId() const;
+};
